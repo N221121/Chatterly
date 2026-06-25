c0b274a85b5548e38a5217452f17c326f8975568
N221121 <n221121@rguktn.ac.in>
2026-06-25 17:03:43 +0530

    signup UI implentend

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,12 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import {ENV} from "./lib/env.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 const app=express();
 
 const PORT =  ENV.PORT|| 3000;
 app.use(express.json());
+app.use(cors({origin: ENV.CLIENT_URL, credentials:true})); // it allows frontend tosend cookies to backend
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
