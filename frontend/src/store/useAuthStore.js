fe012259bd938b74ca3152b3560eca21f107bd52
N221121 <n221121@rguktn.ac.in>
2026-06-25 21:03:39 +0530

    login and logout implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create((set)=>({
    authUser: null,
    isCheckingAuth:true,
    isSigningUp:false,
+   isLoggingIn:false,
 
    checkAuth: async ( )=>{
     try{
@@ -33,5 +34,33 @@ export const useAuthStore = create((set)=>({
     finally{
         set({isSigningUp:false});
     }
-   }
+   },
+
+   login: async(data)=>{
+    set({isLoggingIn:true});
+    try{
+         const res= await axiosInstance.post("/auth/login",data);
+         set({authUser:res.data});
+
+         toast.success("Logged in Successfully");
+    }
+    catch(error){
+        toast.error(error.response.data.message);
+    }
+    finally{
+        set({isLoggingIn:false});
+    }
+   },
+   logout: async()=>{
+    try{
+         await axiosInstance.post("/auth/logout");
+         set({authUser:null});
+
+         toast.success("Logout in Successfully");        
+    }
+    catch(error){
+        toast.error("Error in logging out");
+        console.log("logout error",error);
+    }
+   },
 }));
