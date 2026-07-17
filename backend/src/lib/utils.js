138cac213785a532da6bfc73c4501c8b403558dc
N221121 <n221121@rguktn.ac.in>
2026-07-17 10:07:15 +0530

    changing utils.js file

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,14 +2,16 @@ import jwt from "jsonwebtoken";
 import {ENV} from "./env.js";
 
 export const generateToken = (userId , res)=>{
+    console.log("generateToken called");
     const token = jwt.sign({userId},ENV.JWT_SECRET,{
         expiresIn:"7d",
     });
+       console.log("Token created");
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         secure: ENV.NODE_ENV !== "development",
         sameSite: ENV.NODE_ENV === "development" ? "lax" : "none",
     });
-
+    console.log("Cookie set");
 };
